ca60850896715fd81829f6ff136bc384694c740a
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-10 19:39:55 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,19 @@
+// middleware/auth.js
+
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+};
+
+module.exports = auth;
 
