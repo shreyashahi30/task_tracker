87b6bccedd1120dc532271cf854d6fed7d9b1651
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-11 11:35:11 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,1 +1,19 @@
+// pages/Login.js
+
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
 
