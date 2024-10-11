484ace1541c88cd853a8e52dcc71200e05917351
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-11 11:33:54 +0530

    Update Task.js

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,1 +1,17 @@
+// components/Task.js
+
+import React from 'react';
+
+function Task({ task, onDelete, onUpdate }) {
+  return (
+    <div className="task">
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+      <button onClick={() => onUpdate(task)}>Edit</button>
+      <button onClick={() => onDelete(task._id)}>Delete</button>
+    </div>
+  );
+}
+
+export default Task;
 
