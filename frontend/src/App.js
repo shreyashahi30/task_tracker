01f3d236dfd95808dccac388cac499371fc4655a
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2024-10-11 11:33:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,24 @@
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TaskList from './pages/TaskList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={TaskList} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
